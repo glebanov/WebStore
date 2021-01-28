8d3390151dcb4010717c7c13944749c814b23d9b
Nikolai Glebanov <n.glebanov@gmail.com>
2021-01-28 16:18:40 +0300

    Реализация сервиса управления сотрудниками при их размещении в памяти. Добавлена функция редактирования и удаления сотрудников.

---
+++ WebStore/Infrastructure/Middleware/TestMiddleware.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace WebStore.Infrastructure.Middleware
+{
+    public class TestMiddleware
+    {
+        private readonly RequestDelegate _Next;
+
+        public TestMiddleware(RequestDelegate Next) => _Next = Next;
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+
+            var next = _Next(context);
+
+            await next;
+        }
+    }
+}
