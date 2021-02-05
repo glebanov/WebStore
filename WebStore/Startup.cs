d1eb1ab65b9ff7d920f3caf5a011d7d0a810f7f1
Nikolai Glebanov <n.glebanov@gmail.com>
2021-02-05 15:03:27 +0300

    Новая реализация сервисов каталога товаров SQL-БД

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -9,7 +9,9 @@ using WebStore.Infrastructure.Middleware;
 using WebStore.Infrastructure.Services;
 using WebStore.DAL.Context;
 using Microsoft.EntityFrameworkCore;
-
+using WebStore.Data;
+using WebStore.Infrastructure.Services.InSQL;
+using WebStore.Infrastructure.Services.InMemory;
 
 namespace WebStore
 {
@@ -19,17 +21,21 @@ namespace WebStore
         {
             //services.AddDbContext<WebStoreDB>(opt => opt.UseSqLite(Configuration.GetConnectionString("Sqlite")));//Подключение другого сервера
             services.AddDbContext<WebStoreDB>(opt => opt.UseSqlServer(Configuration.GetConnectionString("Default")));
+            services.AddTransient<WebStoreDbInitializer>();
 
             services.AddTransient<IEmployeesData, InMemoryEmployeesData>();
-            services.AddTransient<IProductData, InMemoryProductData>();
+            //services.AddTransient<IProductData, InMemoryProductData>();
+            services.AddTransient<IProductData, SqlProductData>();
 
             services
                .AddControllersWithViews()
                .AddRazorRuntimeCompilation();
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, WebStoreDbInitializer db)
         {
+            db.Initialize();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
