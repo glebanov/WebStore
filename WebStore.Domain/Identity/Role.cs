7515c91a75b1995558cd41b01e5e5a17a205b64d
Nikolai Glebanov <n.glebanov@gmail.com>
2021-02-05 19:40:41 +0300

    Сущности "Посльзователь " и роль для системы

---
+++ WebStore.Domain/Identity/Role.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WebStore.Domain.Identity
+{
+    public class Role : IdentityRole
+    {
+        public const string Administrator = "Administrators";
+
+        public const string Users = "Users";
+
+        public string Description { get; set; }
+    }
+}
