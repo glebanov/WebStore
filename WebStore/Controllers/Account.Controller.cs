a2f1ab299e6feba600c1f31b9ea93d7427997e2e
Nikolai Glebanov <n.glebanov@gmail.com>
2021-02-07 13:37:36 +0300

    Добавлены данные в Login

--- WebStore/Controllers/Account.Controller.cs
+++ WebStore/Controllers/Account.Controller.cs
@@ -16,12 +16,13 @@ namespace WebStore.Controllers
             _UserManager = UserManeger;
             _SignInManager = SignInManager;
         }
+        #region Register
 
-         public IActionResult Register() => View(new RegisterUserViewModel());
-      
+        public IActionResult Register() => View(new RegisterUserViewModel());
 
-        [HttpPost,ValidateAntiForgeryToken]
-        public async Task<ActionResult> Register (RegisterUserViewModel Model)
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<ActionResult> Register(RegisterUserViewModel Model)
         {
             if (!ModelState.IsValid) return View(Model);
 
@@ -43,6 +44,39 @@ namespace WebStore.Controllers
 
             return View(Model);
         }
-  
+        #endregion
+
+        #region Login
+        public IActionResult Login(string ReturnUrl) => View(new LoginViewModel { ReturnUrl = ReturnUrl });
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel Model)
+        {
+            if (!ModelState.IsValid) return View(Model);
+            var login_result = await _SignInManager.PasswordSignInAsync(
+                Model.UserName,
+                Model.Password,
+                Model.RememberMe,
+#if DEBUG
+
+                false
+
+#else
+                true
+#endif
+                );
+            if (login_result.Succeeded)
+            {
+                return LocalRedirect(Model.ReturnUrl ?? "/");
+            }
+
+            ModelState.AddModelError("", "Неверное имя пользователя, или пароль!");
+
+            return View(Model);
+        } 
+        #endregion
+
     }
+
 }
+
