8e99b044c3b6fd41ba223c6b21a75efd21f0b4b4
Nikolai Glebanov <n.glebanov@gmail.com>
2021-02-12 16:56:04 +0300

    Добавлены Cookies

---
+++ WebStore/Infrastructure/Interfaces/ICartService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.ViewModels;
+
+namespace WebStore.Infrastructure.Interfaces
+{
+    public interface ICartService
+    {
+        void Add(int id); //Добавление товара
+
+        void Decrement(int id); //Уменьшение товара
+
+        void Remove(int id); //Удаление товара
+
+        void Clear();
+
+        CartViewModel GetViewModel();
+    }
+}
---
+++ WebStore/Infrastructure/Services/InCookies/InCookiesCartService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using WebStore.Infrastructure.Interfaces;
+using WebStore.ViewModels;
+
+namespace WebStore.Infrastructure.Services.InCookies
+{
+    public class InCookiesCartService : ICartService
+    {
+        private readonly IHttpContextAccessor _HttpContextAccessor;
+        private readonly string _CartName;
+
+        public InCookiesCartService(IHttpContextAccessor HttpContextAccessor)
+        {
+            this._HttpContextAccessor = HttpContextAccessor;
+        }
+
+        public void Add(int id) => throw new NotImplementedException();
+        public void Clear() => throw new NotImplementedException();
+        public void Decrement(int id) => throw new NotImplementedException();
+        public CartViewModel GetViewModel() => throw new NotImplementedException();
+        public void Remove(int id) => throw new NotImplementedException();
+
+    }
+}
